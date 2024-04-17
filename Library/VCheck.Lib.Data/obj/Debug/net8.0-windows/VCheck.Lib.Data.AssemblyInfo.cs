39dfe34b22cb3c76c62429899a99a8a81fcecb1e
Muhammad Azwan Masri <azwan@svrtech.com.my>
2024-04-17 09:26:28 +0800

    Push latest code

--- Library/VCheck.Lib.Data/obj/Debug/net8.0-windows/VCheck.Lib.Data.AssemblyInfo.cs
+++ Library/VCheck.Lib.Data/obj/Debug/net8.0-windows/VCheck.Lib.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VCheck.Lib.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+147175fe510551881f626398f1c357c5d7482aa7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88a6c3aec525678ec40619caec9ced6073fd619a")]
 [assembly: System.Reflection.AssemblyProductAttribute("VCheck.Lib.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VCheck.Lib.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
