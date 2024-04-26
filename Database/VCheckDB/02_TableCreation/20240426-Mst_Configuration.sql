423983547f9d3c2dde6cec438b81b6f783ac842e
Muhammad Azwan Masri <azwan@svrtech.com.my>
2024-04-26 10:27:50 +0800

    latest push

---
+++ Database/VCheckDB/02_TableCreation/20240426-Mst_Configuration.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `mst_configuration` (
+  `ConfigurationID` int NOT NULL AUTO_INCREMENT,
+  `ConfigurationKey` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `ConfigurationValue` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`ConfigurationID`,`ConfigurationKey`,`ConfigurationValue`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
