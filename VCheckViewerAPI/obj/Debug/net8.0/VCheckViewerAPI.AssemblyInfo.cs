8ffd3142f554426767db36d8a893b4c83d403ce6
SVRTechKenny <kenny@svrtech.com.my>
2024-05-28 11:46:27 +0800

    code merged

--- VCheckViewerAPI/obj/Debug/net8.0/VCheckViewerAPI.AssemblyInfo.cs
+++ VCheckViewerAPI/obj/Debug/net8.0/VCheckViewerAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VCheckViewerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc3cedebfb94426b5b7d0cac9bcf1a02fb17a972")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7c59563454c98def242459d4c3177f219767402")]
 [assembly: System.Reflection.AssemblyProductAttribute("VCheckViewerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VCheckViewerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
