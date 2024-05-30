de76fa224ed93e0798a5f431eee3d3350c9bd9a7
Muhammad Azwan Masri <azwan@svrtech.com.my>
2024-05-30 10:10:11 +0800

    push before merge

--- VCheckViewer/obj/Debug/net8.0-windows/VCheckViewer.AssemblyInfo.cs
+++ VCheckViewer/obj/Debug/net8.0-windows/VCheckViewer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VCheckViewer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+021bc90f432b644106516ddc34901b33c232db25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5a98fe2b43cec8ba6fbec6a2d662fc00a7e5e68")]
 [assembly: System.Reflection.AssemblyProductAttribute("VCheckViewer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VCheckViewer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
