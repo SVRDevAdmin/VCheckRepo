5d99a03bf6974b4ec64f8e1ccbc5b101dfc57df7
Muhammad Azwan Masri <azwan@svrtech.com.my>
2024-06-10 08:25:33 +0800

    pushing latest codes

--- VCheckViewer/obj/Debug/net8.0-windows/VCheckViewer.AssemblyInfo.cs
+++ VCheckViewer/obj/Debug/net8.0-windows/VCheckViewer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VCheckViewer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82a290e027b7eeef16b48776828b62b26ae5a059")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+932d72347f3ab18e362a18faeb56d8b7baacdb70")]
 [assembly: System.Reflection.AssemblyProductAttribute("VCheckViewer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VCheckViewer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
