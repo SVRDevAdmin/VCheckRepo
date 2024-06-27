0e2860b00942ec76162a4ada392b3f779791ef50
SVRTechKenny <kenny@svrtech.com.my>
2024-06-27 13:31:37 +0800

    check-in latest changes

--- Library/VCheck.Lib.Data/obj/Debug/net8.0-windows/VCheck.Lib.Data.AssemblyInfo.cs
+++ Library/VCheck.Lib.Data/obj/Debug/net8.0-windows/VCheck.Lib.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VCheck.Lib.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0910009336e4bc3a362ff8264a15f30701023e2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79a8f1eed7ec917d15d4858720b4108a9f8126fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("VCheck.Lib.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VCheck.Lib.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- VCheckListenerWorker/obj/Debug/net8.0/VCheckListenerWorker.AssemblyInfo.cs
+++ VCheckListenerWorker/obj/Debug/net8.0/VCheckListenerWorker.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VCheckListenerWorker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0910009336e4bc3a362ff8264a15f30701023e2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79a8f1eed7ec917d15d4858720b4108a9f8126fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("VCheckListenerWorker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VCheckListenerWorker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
