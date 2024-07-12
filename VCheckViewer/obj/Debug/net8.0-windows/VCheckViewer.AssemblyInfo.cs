557ede0c9977b3d7b4f1cdb385cc06aa2c6f92c1
SVRTechKenny <kenny@svrtech.com.my>
2024-07-12 09:14:44 +0800

    check-in new favicon uses

--- VCheckViewer/obj/Debug/net8.0-windows/VCheckViewer.AssemblyInfo.cs
+++ VCheckViewer/obj/Debug/net8.0-windows/VCheckViewer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VCheckViewer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.1")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0.1+328fa4dd9b30e3ff508eb559a76f4c932324fdcf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0.1+1c0d9c3a81e08ffbd29edf4fb3b9255a4437ebc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("VCheckViewer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VCheckViewer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.1")]
