cf7ebbf4911e3439a5324250462e7ef541cf03e1
SVRTechKenny <kenny@svrtech.com.my>
2024-07-12 09:17:26 +0800

    merged new changes

--- Library/VCheck.Helper/obj/Debug/net8.0-windows/VCheck.Helper.AssemblyInfo.cs
+++ Library/VCheck.Helper/obj/Debug/net8.0-windows/VCheck.Helper.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VCheck.Helper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c0d9c3a81e08ffbd29edf4fb3b9255a4437ebc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+557ede0c9977b3d7b4f1cdb385cc06aa2c6f92c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("VCheck.Helper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VCheck.Helper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- VCheckViewerAPI/obj/Debug/net8.0/VCheckViewerAPI.AssemblyInfo.cs
+++ VCheckViewerAPI/obj/Debug/net8.0/VCheckViewerAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VCheckViewerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+328fa4dd9b30e3ff508eb559a76f4c932324fdcf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+557ede0c9977b3d7b4f1cdb385cc06aa2c6f92c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("VCheckViewerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VCheckViewerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
