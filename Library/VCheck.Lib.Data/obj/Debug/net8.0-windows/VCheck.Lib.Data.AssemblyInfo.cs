b1de1c23b1cfc3abdefef83957cc19782e33bfcf
SVRTechKenny <kenny@svrtech.com.my>
2024-10-10 09:49:04 +0800

    check-in code fixes

--- Library/VCheck.Lib.Data/obj/Debug/net8.0-windows/VCheck.Lib.Data.AssemblyInfo.cs
+++ Library/VCheck.Lib.Data/obj/Debug/net8.0-windows/VCheck.Lib.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VCheck.Lib.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebd5773831a774e11e69287ddd2aae6f19a7599c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+497daaff6a0091ace4b41619e15227dd228dfa43")]
 [assembly: System.Reflection.AssemblyProductAttribute("VCheck.Lib.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VCheck.Lib.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
